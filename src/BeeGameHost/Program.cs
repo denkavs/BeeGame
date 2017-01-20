37d393b2ef69e73e30d609058796dab948ebac98
DenkaVS <dv.starcrab@gmail.com>
2017-01-20 15:33:11 +0200

    Configured BeeGameHost before including controller

--- src/BeeGameHost/Program.cs
+++ src/BeeGameHost/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BeeGameHost.Infrastructure;
+using Microsoft.Owin.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ src/BeeGameHost/Startup.cs
@@ -0,0 +1,31 @@
+﻿using BeeGameHost.Infrastructure;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BeeGameHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.DependencyResolver = new NinjectResolver();
+
+            config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
